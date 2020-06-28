da5c3e5b87008ebece6359f907e793994b4e8d34
mwmcnall <mwmcnall@gmail.com>
2020-06-28 13:26:35 -0500

    Added Syntax_Error

---
+++ Syntax_Error.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef SYNTAX_ERROR_H
+#define SYNTAX_ERROR_H
+
+#include <stdexcept>
+
+class Syntax_Error : public std::invalid_argument
+{
+public:
+	Syntax_Error(std::string msg) : std::invalid_argument(msg) {}
+};
+
+#endif
